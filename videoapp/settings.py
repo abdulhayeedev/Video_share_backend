f1ba2e775b7f4208f88f7a1089ed4e06860dc9f9
Abdul Hayee <hayee20112@gmail.com>
2025-08-26 21:05:06 +0500

    Update settings.py

--- videoapp/settings.py
+++ videoapp/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'videoapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'videoshare'),
+        'USER': os.environ.get('POSTGRES_USER', 'abdulhayee'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', '12341233'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
 
